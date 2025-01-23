b4b6ceb19d737ebee0ea85cf89d38fc80a30c924
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-23 15:08:35 +0530

    Passing Components as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
 
-const Greeting=({name})=>{// Destructuring props inside functional component's parameters
+const Greeting=({name})=>{ // Destructuring props inside functional component's parameters
 
 return <h1>Hi {name}</h1>
 }
 const IndianGreeting=({name})=>{
 return <h1>Namaste {name}</h1>
+}
+function Parent({children}){// Passing Children as Props
+  return(
+    <div style={{padding:"1rem",backgroundColor:"grey",color:"white"}}>
+     <p> Hi,I am parent!</p>
+     <p>This is my Child</p>
+     <p>{children}</p>  
+    </div>
+  )
 }
 function App() {
 
@@ -15,7 +24,9 @@ function App() {
     <>
      <Greeting name="React Learner"/>
      <IndianGreeting name="React Learner"/>
-    
+    <Parent>
+      <IndianGreeting name="Karan"/> 
+      </Parent>     
     </>
   )
 }
